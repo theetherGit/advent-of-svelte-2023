4a1726ad0168735d437c850f057514fb5abaa309
Shivam Meena <meenashivam9650@gmail.com>
2023-12-04 09:36:37 +0530

    Google Analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,31 @@
 	import { ModeWatcher } from 'mode-watcher';
 	import { Toaster } from 'sonner-svelte';
 	import Navbar from "$lib/components/extended-ui/Navbar.svelte";
+	import { partytownSnippet } from '@builder.io/partytown/integration';
 </script>
+
+<svelte:head>
+    <script>
+		partytown = {
+			forward: ['dataLayer.push']
+		};
+    </script>
+
+    {@html '<script>' + partytownSnippet() + '</script>'}
+
+    <script
+            type="text/partytown"
+            src="https://www.googletagmanager.com/gtag/js?id=G-PGXN0M6LF0"
+    ></script>
+    <script type="text/partytown">
+		window.dataLayer = window.dataLayer || [];
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
+		gtag('config', 'G-PGXN0M6LF0');
+    </script>
+</svelte:head>
 
 <ModeWatcher />
 <div class="bg-background h-full">
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { join } from 'node:path';
+import { partytownVite } from '@builder.io/partytown/utils';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		partytownVite({
+			// `dest` specifies where files are copied to in production
+			dest: join(process.cwd(), 'static', '~partytown')
+		})
+	]
 });
